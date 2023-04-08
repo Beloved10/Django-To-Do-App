3d29b7878ffa96d9b126199b543cd3cc5372ea5e
Ikhide Augustine <augustineimoleme@gmail.com>
2023-04-08 16:34:32 +0100

    Deploying to railway

--- todo/settings.py
+++ todo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-de&)oyri7k1vlsb9c3=cc!d+km=$719dgw=&396^p70p1lhwt@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
